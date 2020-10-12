9dd10fadf085d8b60c202055e7847ba1a1948d24
julienseux <julienseux@leschassagnes.net>
2020-10-12 14:37:51 +0200

    Section_2 Final

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+
+Route::get('/',function (){
     return view('welcome');
 });
+
+Route::get('medicaments','App\Http\Controllers\MedicamentController@medicaments');
+Route::get('praticiens','App\Http\Controllers\PraticienController@praticiens');
